6895e3ff236bcb68930355ba40dd025073556def
WONG Tsz Yau <wong.tszyau@systemi.co.jp>
2023-07-20 10:51:04 +0900

    起動時ブラウザを自動で開く設定を追加

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     host: true,
     watch: {
-      usePolling: true
+      usePolling: true,
     },
+    open: true,
   },
-})
+});
