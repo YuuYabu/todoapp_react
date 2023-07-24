421a6d5ee0d4a6db98b77fd3a10d033144cca563
WONG Tsz Yau <wong.tszyau@systemi.co.jp>
2023-07-24 17:53:53 +0900

    モック作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,196 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import DatePicker, { registerLocale } from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import ja from "date-fns/locale/ja";
 
+const getTodayString = (): string => {
+  let today = new Date();
+  return (
+    today.getFullYear().toString() +
+    "-" +
+    ("0" + (today.getMonth() + 1).toString()).slice(-2) +
+    "-" +
+    today.getDate().toString()
+  );
+};
+type Todo = {
+  id: number;
+  title: string;
+  currentStatus: number;
+  endAt: string;
+  deletedAt: string | null;
+};
+const emptyTodo: Todo = {
+  id: 0,
+  title: "",
+  currentStatus: 0,
+  endAt: getTodayString(),
+  deletedAt: null,
+};
+const todo1: Todo = {
+  id: 1,
+  title: "Test1",
+  currentStatus: 0,
+  endAt: "2023-07-25",
+  deletedAt: null,
+};
+const todo2: Todo = {
+  id: 2,
+  title: "Test2",
+  currentStatus: 0,
+  endAt: "2023-07-30",
+  deletedAt: "2023-07-20",
+};
+const todo3: Todo = {
+  id: 3,
+  title: "Test3",
+  currentStatus: 0,
+  endAt: "2023-07-23",
+  deletedAt: null,
+};
+const statusNameList: { [key: number]: string } = {
+  0: "未着手",
+  1: "作業中",
+  2: "完了",
+};
+const buttonClassList: { [key: number]: string } = {
+  0: "outline-secondary",
+  1: "outline-warning",
+  2: "outline-info",
+};
 function App() {
-  const [count, setCount] = useState(0);
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+  useEffect(() => {
+    setTodoList([todo1, todo2, todo3]);
+  }, []);
+
+  const getButtonClass = (todo: Todo): string => {
+    return buttonClassList[todo.currentStatus];
+  };
+  const getStatusName = (todo: Todo): string => {
+    return statusNameList[todo.currentStatus];
+  };
+
+  const [newTodo, setNewTodo] = useState<Todo>(emptyTodo);
+  const handleChangeNewTodoTitle = (e: any) => {
+    setNewTodo({ ...newTodo, title: e.target.value });
+  };
+  const handleChangeNewTodoEndAt = (e: any) => {
+    newTodo.endAt = e.target.value;
+    setNewTodo({ ...newTodo, endAt: e.target.value });
+  };
+  const handleSubmitNewTodo = (e: any) => {
+    e.preventDefault();
+    let maxId: number = 0;
+    todoList.forEach((todo: Todo) => {
+      if (!maxId || todo.id > maxId) {
+        maxId = todo.id;
+      }
+    });
+    todoList.push(Object.assign({}, newTodo, { id: maxId + 1 }));
+    setTodoList([...todoList]);
+    setNewTodo(emptyTodo);
+  };
+  registerLocale("ja", ja);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Todo</h1>
+      <h2>New Todo</h2>
+      <form className="row" onSubmit={handleSubmitNewTodo}>
+        <div className="col-4">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="title"
+            value={newTodo.title}
+            onChange={handleChangeNewTodoTitle}
+            required={true}
+          />
+        </div>
+        <div className="col-4">
+          <DatePicker
+            onChange={handleChangeNewTodoEndAt}
+            dateFormat="yyyy/MM/dd"
+            selected={new Date()}
+            minDate={new Date()}
+            locale="ja"
+            className="form-control"
+          />
+        </div>
+        <div className="col-2">
+          <Button variant="primary" type="submit">
+            登録
+          </Button>
+        </div>
+      </form>
+      <h2>Todo List</h2>
+      {todoList.map((todo) => {
+        return (
+          <form className="row" key={todo.id}>
+            <div className="col-4">
+              <input
+                type="text"
+                className="form-control"
+                defaultValue={todo.title}
+                style={{
+                  textDecoration:
+                    todo.currentStatus === 2 ? "line-through" : "",
+                }}
+                readOnly={todo.currentStatus === 2}
+              />
+            </div>
+            <div className="col-4">
+              {/* <input
+                type="date"
+                className="form-control"
+                value={todo.endAt}
+                readOnly={todo.currentStatus === 2}
+              /> */}
+              <DatePicker
+                className="form-control"
+                minDate={new Date()}
+                locale="ja"
+                dateFormat="yyyy/MM/dd"
+                selected={new Date(todo.endAt)}
+                onChange={(selectedDate) => {
+                  todo.endAt =
+                    selectedDate?.toDateString() || new Date().toDateString();
+                }}
+              />
+            </div>
+            <div className="col-2">
+              <Button
+                variant="success"
+                onClick={(e) => {
+                  e.preventDefault();
+                  console.log(todo);
+                }}
+              >
+                更新
+              </Button>
+            </div>
+            <div className="col-2">
+              <Button
+                variant={getButtonClass(todo)}
+                onClick={(e) => {
+                  e.preventDefault();
+                  todo.currentStatus = Math.min(todo.currentStatus + 1, 2);
+                  const tList = todoList.map((t) =>
+                    t.id === todo.id ? todo : t
+                  );
+                  setTodoList(tList);
+                }}
+              >
+                {getStatusName(todo)}
+              </Button>
+            </div>
+          </form>
+        );
+      })}
     </>
   );
 }
