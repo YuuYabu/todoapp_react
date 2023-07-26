30816cc824d286ebb03ccd57461536467b723c10
WONG Tsz Yau <wong.tszyau@systemi.co.jp>
2023-07-26 11:22:14 +0900

    Todoの全リスト取得メソッドを実装

--- backend/src/main/java/com/todo/app/controller/TodoController.java
+++ backend/src/main/java/com/todo/app/controller/TodoController.java
@@ -1,14 +1,27 @@
 package com.todo.app.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.todo.app.entity.Todo;
+import com.todo.app.service.TodoService;
+
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class TodoController {
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
     @GetMapping("/todo")
-    public String getAll() {
-        return "aaa";
+    public List<Todo> getAll() {
+        return this.todoService.allTodos();
     }
 }
