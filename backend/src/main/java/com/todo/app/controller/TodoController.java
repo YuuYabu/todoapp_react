9c4dda20fd2ccfd5e3746241ff9b15af076ac440
WONG Tsz Yau <wong.tszyau@systemi.co.jp>
2023-07-26 10:04:33 +0900

    サンプルコントローラーを追加

---
+++ backend/src/main/java/com/todo/app/controller/TodoController.java
@@ -0,0 +1,14 @@
+package com.todo.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TodoController {
+    @GetMapping("/todo")
+    public String getAll() {
+        return "aaa";
+    }
+}
