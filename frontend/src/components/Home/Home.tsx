bee29842218f7aa0577f6810d2c02093cb99668a
WONG Tsz Yau <wong.tszyau@systemi.co.jp>
2023-07-27 12:00:45 +0900

    フィルター機能を実装

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import { TodoType } from "../../App";
 import TodoList from "../TodoList/TodoList";
 import Add from "../Add/Add";
+import FilterList from "../FilterList/FilterList";
 
 interface Props {
   todoList: TodoType[];
@@ -11,17 +12,33 @@ interface Props {
   deleteTodo: Function;
 }
 
+export type FilterTypeContextType = {
+  filterType: number;
+  setFilterType: (filterType: number) => void;
+};
+
+export const FilterTypeContext = createContext<FilterTypeContextType>({
+  filterType: 0,
+  setFilterType: (filterType) => {},
+});
+
 const Home: React.FC<Props> = (props) => {
   const todoList = props.todoList;
+  const filter = 3;
+  const [filterType, setFilterType] = useState<number>(3);
   return (
     <>
       <h1>Todo App</h1>
       <Add todoList={todoList} addTodo={props.addTodo} />
+      <FilterTypeContext.Provider value={{ filterType, setFilterType }}>
+        <FilterList filter={filter}></FilterList>
+      </FilterTypeContext.Provider>
       <TodoList
         todoList={todoList}
         updateTodo={props.updateTodo}
         progressTodo={props.progressTodo}
         deleteTodo={props.deleteTodo}
+        filter={filterType}
       />
     </>
   );
