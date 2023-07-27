c732d57d12a4185efe53b3f95a1ae0c17e00ef00
WONG Tsz Yau <wong.tszyau@systemi.co.jp>
2023-07-27 17:38:46 +0900

    ページネーションを追加

--- frontend/src/components/TodoList/TodoList.tsx
+++ frontend/src/components/TodoList/TodoList.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { TodoType } from "../../App";
 import Todo from "../Todo/Todo";
+import ReactPaginate from "react-paginate";
 
 interface Props {
   todoList: TodoType[];
@@ -13,6 +14,7 @@ interface Props {
 const TodoList: React.FC<Props> = (props) => {
   const todoList = props.todoList;
   let filteredTodoList: TodoType[];
+
   if (props.filter !== 4) {
     filteredTodoList = todoList
       .filter((todo) =>
@@ -22,11 +24,43 @@ const TodoList: React.FC<Props> = (props) => {
   } else {
     filteredTodoList = todoList.filter((todo) => todo.deletedAt !== null);
   }
+
+  const itemsPerPage = 5;
+  const [itemsOffset, setItemsOffset] = useState(0);
+  const endOffset = itemsOffset + itemsPerPage;
+  const currentTodoList = filteredTodoList.slice(itemsOffset, endOffset);
+  const pageCount = Math.ceil(filteredTodoList.length / itemsPerPage);
+
+  const handlePageClick = (e: { selected: number }) => {
+    const newOffset = (e.selected * itemsPerPage) % filteredTodoList.length;
+    setItemsOffset(newOffset);
+  };
+
   return (
     <>
       <h2>Todo List</h2>
+      <ReactPaginate
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        nextLabel="next >"
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={2}
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakLabel="..."
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+        className="pagination"
+      />
       <ul className="list-group list-group-flush">
-        {filteredTodoList.map((todo) => {
+        {currentTodoList.map((todo) => {
           return (
             <li className="list-group-item" key={todo.id}>
               <Todo
